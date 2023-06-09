104100fd1434c44278296ce459bb963b3f3c7c65
hukurou123 <k23125kk@aitec.ac.jp>
2023-06-09 12:52:45 +0900

    文字列

--- arrangement.c
+++ arrangement.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-	char str[4];
+	char str[5];
 	int count;
 	str[0] = 'R';
 	str[1] = 'e';
 	str[2] = 'n';
 	str[3] = 'a';
-	for(count=0;count<=3;count++) printf("%c",str[count]);
-	printf("\n");
+	str[4] = 0;
+	printf(str);
 	return 0;
 
 }
